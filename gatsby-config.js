ac05fde6f7a138b065a21e4123c31413059a94c7
Mr. Robot <joselsondias@hotmail.com>
2019-07-09 22:46:53 +0100

    Adding all files

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'JDMusic',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Oiça as suas músicas preferidas</h1>
+    <p>A qualquer hora.</p>
+    <p>e em qualquer lugar</p>
+    <Link to="/page-2/">Ir para pagina 2</Link>
+  </div>
+)
+
+export default IndexPage
