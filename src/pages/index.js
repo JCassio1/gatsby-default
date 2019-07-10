987d30f061bad93de6ff7ef137943eb88f45ab6a
Mr. Robot <joselsondias@hotmail.com>
2019-07-10 04:08:58 +0100

    Style index page with CSS stylesheet, grid and main elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Oiça as suas músicas preferidas</h1>
-    <p>A qualquer hora.</p>
-    <p>e em qualquer lugar</p>
-    <Link to="/page-2/">Ir para pagina 2</Link>
+
+    <div className="Hero">
+      <div className="HeroGroup">
+          <h1>Oiça as suas músicas preferidas</h1>
+          <p>A qualquer hora.</p>
+          <p>e em qualquer lugar</p>
+          <Link to="/page-2/">Assistir Video</Link>
+      </div>
+    </div>
   </div>
 )
 
